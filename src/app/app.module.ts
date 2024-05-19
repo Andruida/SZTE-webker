dd8546735e7d3c2ef540b0cec2c84164655edee1
Andruida <41823729+Andruida@users.noreply.github.com>
2024-05-19 05:46:57 +0200

    Basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({"projectId":"biztos-kocsi","appId":"1:2149388697:web:24939f569233fb522b0308","storageBucket":"biztos-kocsi.appspot.com","apiKey":"AIzaSyBcXSMHj3oAlW0lCSu8dzJKzh9XMWKERzY","authDomain":"biztos-kocsi.firebaseapp.com","messagingSenderId":"2149388697"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
